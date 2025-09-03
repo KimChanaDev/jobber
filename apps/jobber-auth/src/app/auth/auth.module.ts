4b7e8a7421f0bce8c2b66c6eb6aeabb66b581e32
KimChanaDev <chanaweestr@gmail.com>
2025-09-03 20:03:29 +0700

    feat: implement authentication module with JWT support

---
+++ apps/jobber-auth/src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigService],
+      inject: [ConfigService], // Inject the ConfigService to useFactory
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow('JWT_EXPIRATION_MS'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthResolver, AuthService],
+})
+export class AuthModule {}
