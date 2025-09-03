064e5df6f69a726adc786508580f191e6b8bfbe6
KimChanaDev <chanaweestr@gmail.com>
2025-09-03 18:37:25 +0700

    refactor: fix ci test: nx run @jobber/nestjs:typecheck

--- apps/jobber-auth/src/app/users/models/user.model.ts
+++ apps/jobber-auth/src/app/users/models/user.model.ts
@@ -1,8 +1,8 @@
 import { AbstractModel } from '@jobber/nestjs';
-import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class UserModel extends AbstractModel {
-    @Field()
-    email!: string;
+  @Field()
+  email!: string;
 }
