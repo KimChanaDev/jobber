0dd98083fd004d26c71ca573dc9e59f15bea635c
KimChanaDev <chanaweestr@gmail.com>
2025-09-04 19:17:20 +0700

    feat: add jobber-jobs and jobber-jobs-e2e applications with initial setup, including controllers, services, and tests

---
+++ apps/jobber-jobs/src/main.ts
@@ -0,0 +1,21 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  // all request will reject the body if not matching the dto
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix(globalPrefix);
+  app.use(cookieParser());
+  const port = app.get(ConfigService).getOrThrow('PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
